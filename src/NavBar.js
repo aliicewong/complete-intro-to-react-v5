cb8f62728e44bbad9cc5d5f2144163afc72d97ab
Alice <alice.sky.wong@gmail.com>
2020-12-22 17:51:14 -0800

    part 1

---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "@reach/router";
+import { css } from "@emotion/core";
+
+const NavBar = () => {
+  return (
+    <header
+      css={css`
+        background-color: #333;
+        padding: 15px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span role="img" aria-label="logo">
+        HELLO
+      </span>
+    </header>
+  );
+};
+
+export default NavBar;
